ee33f421381b8763f340be557d0c76fa3d7b8aa4
Cristhian G <cristhian@blitzm.com>
2019-05-29 14:18:30 +1000

    Adding cocoa pods-google maps for IOS, adding firebase calls

--- App.tsx
+++ App.tsx
@@ -9,36 +9,76 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, Button} from 'react-native';
 import FetchLocation from './components/FetchLocation';
 import UsersMap from './components/UsersMap';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 interface Props {}
 export default class App extends Component<Props> {
-
+  constructor(props:any){
+    super(props);
+    this.handleGetUserPlaces = this.handleGetUserPlaces.bind(this);
+  }
+  state = {
+    userLocation: null,
+    usersPlacesArray: []
+  }
   getUserLocationHandler = () => {
-    
     navigator.geolocation.getCurrentPosition(position => {
       console.log('my position', position);
+      this.setState({
+        userLocation:{
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }
+      });
+      fetch('https://woven-nimbus-242100.firebaseio.com/places.json',{
+        method: 'POST',
+        body: JSON.stringify({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
     },
     err => {
       console.log(err);
     });
   }
 
+  handleGetUserPlaces(){
+    fetch('https://woven-nimbus-242100.firebaseio.com/places.json')
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placesArray = [];
+        for (const key in parsedRes){
+            placesArray.push({
+              latitude: parsedRes[key].latitude,
+              longitude: parsedRes[key].longitude,
+              id: key
+            });
+        }
+        this.setState({
+          usersPlacesArray: placesArray
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text >It works!</Text>
+        <View style={{marginBottom: 20}}>
+          <Button title='Get User Places' onPress={this.handleGetUserPlaces}/>
+        </View>
         <FetchLocation onGetLocation={this.getUserLocationHandler}/>
-        <UsersMap></UsersMap>
+        <UsersMap
+          userLocation = {this.state.userLocation}
+          usersPlaces = {this.state.usersPlacesArray}
+        ></UsersMap>
       </View>
     );
   }
--- components/UsersMap.tsx
+++ components/UsersMap.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 interface Props {
-
+    userLocation: any;
+    usersPlaces: any;
 }
-
 
 export default class UsersMap extends React.Component<Props>{
     render() {
+        let userLocationMarker = null;
+        if (this.props.userLocation) {
+            userLocationMarker = <Marker coordinate={this.props.userLocation} />;
+        }
+        let usersMarkers = null;
+        if (this.props.usersPlaces) {
+            usersMarkers = this.props.usersPlaces.map((userPlace:any) => <Marker coordinate={userPlace} key={userPlace.id} />
+            );
+        }
+        console.log("userPlace",usersMarkers);
         return (
-            <View style = {styles.mapContainer}>
-                <MapView  
-                style = {styles.map}
-                    
-                />
+            <View style={styles.mapContainer}>
+                <MapView
+                    style={styles.map}
+                    provider={PROVIDER_GOOGLE}
+
+                    initialRegion={{
+                        latitude: 35.78825,
+                        longitude: -121.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                    region = {this.props.userLocation}
+                >
+                    {userLocationMarker}
+                    {usersMarkers}
+                </MapView>
             </View>
 
         );
@@ -25,7 +46,8 @@ export default class UsersMap extends React.Component<Props>{
 const styles = StyleSheet.create({
     mapContainer: {
         width: '100%',
-        height: 200
+        height: 200,
+        marginTop: 20
     },
     map: {
         width: '100%',
